2960c2ae42cebea78b68713a22b1dcb4899950c7
Your Name <your-email@example.com>
2026-03-23 14:44:12 +0530

    fix: updated cors origin and session for production

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -16,17 +17,52 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 connectDB();
 app.use(cookieParser());
+console.log("CORS Origin:", process.env.FRONTEND_URL);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,}));
+// app.use(
+//   cors({
+//     origin: [
+//       "https://your-frontend-name.vercel.app", // Yahan apna Vercel wala link daaliye
+//       "http://localhost:5173"                // Taaki local pe bhi chalta rahe
+//     ],
+//     credentials: true
+//   })
+// );
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,}));
+    origin: [
+      "https://event-management-platform-peach.vercel.app", // Aapka actual Vercel link
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+// app.use(
+//   session({
+//     secret: process.env.JWT_SECRET,
+//     resave: false,
+//     saveUninitialized: false,}));
+app.set("trust proxy", 1); // Render backend ke liye zaroori hai
 app.use(
   session({
     secret: process.env.JWT_SECRET,
     resave: false,
-    saveUninitialized: false,}));
+    saveUninitialized: false,
+    cookie: {
+      secure: true, // HTTPS ke liye (Live site pe zaroori hai)
+      sameSite: "none", // Cross-domain (Vercel to Render) ke liye zaroori hai
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+  })
+);
 app.use("/user", userRoute);
 app.use("/event", eventRoute);
 app.use("/booking", bookingRoute);
